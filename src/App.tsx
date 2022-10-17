1746776d667c90d9b8ce82a50c019da56eb9bf03
Rafael Herculano <rafa.lj@hotmail.com>
2022-10-17 10:48:11 -0300

    react router dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import { gql, useLazyQuery } from '@apollo/client';
-import Heading from './components/Heading';
 
 const QUERY_EXAMPLE = gql`
   query GET_CATEGORIES {
@@ -37,7 +36,6 @@ const App: React.FC = () => {
 
   return (
     <div>
-      <Heading />
       <h2>123</h2>
       <button onClick={executeQuery}>Call query</button>
       <button onClick={executeRefetch}>refetch</button>
--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  // Route,
+  Link,
+} from 'react-router-dom';
 import { ApolloClient, InMemoryCache, ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
-import App from './App';
+// import App from './App';
 import theme from './styles/theme';
 import GlobalStyles from './styles/global';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: (
+      <div>
+        <h1>Hello World</h1>
+        <Link to="about">About Us</Link>
+      </div>
+    ),
+  },
+  {
+    path: 'about',
+    element: <div>About</div>,
+  },
+]);
 
 const client = new ApolloClient({
   uri: 'http://localhost:1337/graphql',
@@ -18,7 +40,7 @@ root.render(
   <ThemeProvider theme={theme}>
     <ApolloProvider client={client}>
       <GlobalStyles />
-      <App />
+      <RouterProvider router={router} />
     </ApolloProvider>
   </ThemeProvider>
 );
